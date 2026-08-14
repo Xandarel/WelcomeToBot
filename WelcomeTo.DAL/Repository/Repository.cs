2a5aa20a452792efd5b51b72e0eb1a51e744131f
Александр Бордюг <Aleksandr.Bordyug@evraz.com>
2026-08-14 15:48:49 +0500

    db

--- WelcomeTo.DAL/Repository/Repository.cs
+++ WelcomeTo.DAL/Repository/Repository.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WelcomeTo.DAL.Repository
+﻿namespace WelcomeTo.DAL.Repository
 {
-    internal class Repository
+    public sealed class Repository
     {
+
     }
 }
